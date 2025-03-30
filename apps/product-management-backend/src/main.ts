d94d887d65dff25417aceb33effadad2f661ad47
Franz Bendezu <franzant20@gmail.com>
2025-03-30 07:11:50 -0500

    feat: add Swagger API documentation setup for Products Management API

--- apps/product-management-backend/src/main.ts
+++ apps/product-management-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ProductManagementBackendModule } from './product-management-backend.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductManagementBackendModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DeUna Products API')
+    .setDescription('API for Products Management in DeUna')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
