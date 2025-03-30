e33e8376624c86a572f5b19b66e9873659da879f
Franz Bendezu <franzant20@gmail.com>
2025-03-30 02:25:10 -0500

    feat: add CRUD methods and tests for ProductDatabaseRepository

--- apps/product-management-backend/src/adapters/data/repositories/product-database.repository.spec.ts
+++ apps/product-management-backend/src/adapters/data/repositories/product-database.repository.spec.ts
@@ -5,7 +5,13 @@ import {
   DatabasePool,
 } from '../../../infrastructure/config/database.config';
 import { Product } from '../../../domain/models/product.model';
-import { QUERY_FIND_ALL_PRODUCTS } from './query/product.queries';
+import {
+  QUERY_CREATE_PRODUCT,
+  QUERY_DELETE_PRODUCT,
+  QUERY_FIND_ALL_PRODUCTS,
+  QUERY_FIND_PRODUCT_BY_ID,
+  QUERY_UPDATE_PRODUCT,
+} from './query/product.queries';
 import { IProductRow } from '../interfaces/product-row.interface';
 
 describe('ProductDatabaseRepository', () => {
@@ -112,6 +118,229 @@ describe('ProductDatabaseRepository', () => {
 
       expect(mockPool.query).toHaveBeenCalledWith(QUERY_FIND_ALL_PRODUCTS);
       expect(result).toEqual([]);
+    });
+  });
+
+  describe('findById', () => {
+    it('should return a product if found', async () => {
+      const PRODUCT_ID = '1';
+      const PRODUCT_NAME = 'Product 1';
+      const PRODUCT_DESCRIPTION = 'Description 1';
+      const PRODUCT_PRICE = 100;
+      const PRODUCT_STOCK = 10;
+      const PRODUCT_CREATED_AT = new Date();
+      const PRODUCT_UPDATED_AT = new Date();
+      const mockRow: IProductRow = {
+        id: PRODUCT_ID,
+        name: PRODUCT_NAME,
+        description: PRODUCT_DESCRIPTION,
+        price: PRODUCT_PRICE,
+        stock: PRODUCT_STOCK,
+        createdAt: PRODUCT_CREATED_AT,
+        updatedAt: PRODUCT_UPDATED_AT,
+      };
+      const mockProduct = new Product(
+        PRODUCT_ID,
+        PRODUCT_NAME,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_PRICE,
+        PRODUCT_STOCK,
+        PRODUCT_CREATED_AT,
+        PRODUCT_UPDATED_AT,
+      );
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [mockRow],
+        };
+      });
+
+      const result = await repository.findById(PRODUCT_ID);
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_FIND_PRODUCT_BY_ID, [
+        PRODUCT_ID,
+      ]);
+      expect(result).toEqual(mockProduct);
+    });
+
+    it('should return null if no product is found', async () => {
+      const PRODUCT_ID = '1';
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [],
+        };
+      });
+
+      const result = await repository.findById(PRODUCT_ID);
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_FIND_PRODUCT_BY_ID, [
+        PRODUCT_ID,
+      ]);
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('create', () => {
+    it('should create a product and return it', async () => {
+      const PRODUCT_ID = '1';
+      const PRODUCT_NAME = 'Product 1';
+      const PRODUCT_DESCRIPTION = 'Description 1';
+      const PRODUCT_PRICE = 100;
+      const PRODUCT_STOCK = 10;
+      const PRODUCT_CREATED_AT = new Date();
+      const PRODUCT_UPDATED_AT = new Date();
+      const mockRow: IProductRow = {
+        id: PRODUCT_ID,
+        name: PRODUCT_NAME,
+        description: PRODUCT_DESCRIPTION,
+        price: PRODUCT_PRICE,
+        stock: PRODUCT_STOCK,
+        createdAt: PRODUCT_CREATED_AT,
+        updatedAt: PRODUCT_UPDATED_AT,
+      };
+      const mockProduct = new Product(
+        PRODUCT_ID,
+        PRODUCT_NAME,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_PRICE,
+        PRODUCT_STOCK,
+        PRODUCT_CREATED_AT,
+        PRODUCT_UPDATED_AT,
+      );
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [mockRow],
+        };
+      });
+
+      const result = await repository.create({
+        name: PRODUCT_NAME,
+        description: PRODUCT_DESCRIPTION,
+        price: PRODUCT_PRICE,
+        stock: PRODUCT_STOCK,
+      });
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_CREATE_PRODUCT, [
+        PRODUCT_NAME,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_PRICE,
+        PRODUCT_STOCK,
+      ]);
+      expect(result).toEqual(mockProduct);
+    });
+  });
+
+  describe('updateById', () => {
+    it('should update a product and return it', async () => {
+      const PRODUCT_ID = '1';
+      const PRODUCT_NAME = 'Updated Product';
+      const PRODUCT_DESCRIPTION = 'Updated Description';
+      const PRODUCT_PRICE = 150;
+      const PRODUCT_STOCK = 15;
+      const PRODUCT_CREATED_AT = new Date();
+      const PRODUCT_UPDATED_AT = new Date();
+      const mockRow: IProductRow = {
+        id: PRODUCT_ID,
+        name: PRODUCT_NAME,
+        description: PRODUCT_DESCRIPTION,
+        price: PRODUCT_PRICE,
+        stock: PRODUCT_STOCK,
+        createdAt: PRODUCT_CREATED_AT,
+        updatedAt: PRODUCT_UPDATED_AT,
+      };
+      const mockProduct = new Product(
+        PRODUCT_ID,
+        PRODUCT_NAME,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_PRICE,
+        PRODUCT_STOCK,
+        PRODUCT_CREATED_AT,
+        PRODUCT_UPDATED_AT,
+      );
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [mockRow],
+        };
+      });
+
+      const result = await repository.updateById(PRODUCT_ID, {
+        name: PRODUCT_NAME,
+        description: PRODUCT_DESCRIPTION,
+        price: PRODUCT_PRICE,
+        stock: PRODUCT_STOCK,
+      });
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_UPDATE_PRODUCT, [
+        PRODUCT_NAME,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_PRICE,
+        PRODUCT_ID,
+      ]);
+      expect(result).toEqual(mockProduct);
+    });
+
+    it('should return null if no product is updated', async () => {
+      const PRODUCT_ID = '1';
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [],
+        };
+      });
+
+      const result = await repository.updateById(PRODUCT_ID, {
+        name: 'Updated Product',
+        description: 'Updated Description',
+        price: 150,
+        stock: 15,
+      });
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_UPDATE_PRODUCT, [
+        'Updated Product',
+        'Updated Description',
+        150,
+        PRODUCT_ID,
+      ]);
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('deleteById', () => {
+    it('should delete a product and return true', async () => {
+      const PRODUCT_ID = '1';
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [{}],
+        };
+      });
+
+      const result = await repository.deleteById(PRODUCT_ID);
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_DELETE_PRODUCT, [
+        PRODUCT_ID,
+      ]);
+      expect(result).toBe(true);
+    });
+
+    it('should return false if no product is deleted', async () => {
+      const PRODUCT_ID = '1';
+
+      mockPool.query.mockImplementation(() => {
+        return {
+          rows: [],
+        };
+      });
+
+      const result = await repository.deleteById(PRODUCT_ID);
+
+      expect(mockPool.query).toHaveBeenCalledWith(QUERY_DELETE_PRODUCT, [
+        PRODUCT_ID,
+      ]);
+      expect(result).toBe(false);
     });
   });
 });
