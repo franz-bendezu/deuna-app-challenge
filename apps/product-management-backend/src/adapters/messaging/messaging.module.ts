04e42d64ce9f35043699222ae3a783daa147b04b
Franz Bendezu <franzant20@gmail.com>
2025-03-30 07:05:37 -0500

    feat: import InfrastructureModule in MessagingModule for improved messaging functionality

--- apps/product-management-backend/src/adapters/messaging/messaging.module.ts
+++ apps/product-management-backend/src/adapters/messaging/messaging.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductPublisher } from './publisher/product.publisher';
 import { PRODUCT_PUBLISHER } from '../../domain/constants/injection-tokens';
+import { InfrastructureModule } from '../../infrastructure/infraestructure.module';
 
 @Module({
+  imports: [InfrastructureModule],
   providers: [
     {
       provide: PRODUCT_PUBLISHER,
