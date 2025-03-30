df23aa7d36eaa89f6651608efc7c8d0ff523d613
Franz Bendezu <franzant20@gmail.com>
2025-03-30 06:01:10 -0500

    fix: update IProductPublisher interface to correct 'this' context in publish method

--- apps/product-management-backend/src/domain/interfaces/event-publisher.interface.ts
+++ apps/product-management-backend/src/domain/interfaces/event-publisher.interface.ts
@@ -2,5 +2,5 @@ import { ProductEvents } from '../constants/events';
 import { Product } from '../models/product.model';
 
 export interface IProductPublisher {
-  publish(tsopic: ProductEvents, message: Product): Promise<void>;
+  publish(this: void, tsopic: ProductEvents, message: Product): Promise<void>;
 }
