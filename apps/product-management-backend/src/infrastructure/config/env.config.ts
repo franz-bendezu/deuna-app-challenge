c2633781aa02c4827be855211efe0fa6aa3bc626
Franz Bendezu <franzant20@gmail.com>
2025-03-29 23:33:08 -0500

    feat: add Redis configuration to environment settings

--- apps/product-management-backend/src/infrastructure/config/env.config.ts
+++ apps/product-management-backend/src/infrastructure/config/env.config.ts
@@ -8,4 +8,9 @@ export default (): EnvironmentConfig => ({
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'product_management',
   },
+  redis: {
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    ttl: parseInt(process.env.REDIS_TTL || '60000'),
+    lruSize: parseInt(process.env.REDIS_LRU_SIZE || '5000'),
+  },
 });
