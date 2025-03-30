7a42127cec756ee480173fd9ad27c0d36ff7097b
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-30 18:01:58 +0000

    feat(bff): setup graphql config

--- apps/product-management-bff/src/product-management-bff.module.ts
+++ apps/product-management-bff/src/product-management-bff.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductManagementBffController } from './product-management-bff.controller';
 import { ProductManagementBffService } from './product-management-bff.service';
+import {
+  ApolloFederationDriverConfig,
+  ApolloFederationDriver,
+} from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      typePaths: ['**/*.graphql'],
+    }),
+  ],
   controllers: [ProductManagementBffController],
   providers: [ProductManagementBffService],
 })
