6f72c6c2831f12e044db5a859a27fdc4e772e9be
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-04-01 00:37:20 +0000

    refactor: simplify BaseProductDto by extending PartialType from CreateProductDto

--- apps/product-management-backend/src/adapters/web/dtos/base-product.dto.ts
+++ apps/product-management-backend/src/adapters/web/dtos/base-product.dto.ts
@@ -1,46 +1,10 @@
 import { IBaseSharedProduct } from '@app/shared';
-import { Optional } from '@nestjs/common';
-import { ApiProperty, ApiSchema } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import { ApiSchema, PartialType } from '@nestjs/swagger';
+import { CreateProductDto } from './create-product.dto';
 
 @ApiSchema({
   description: 'DTO base para representar un producto en el sistema',
 })
-export class BaseProductDto implements IBaseSharedProduct {
-  @IsString()
-  @Optional()
-  @ApiProperty({
-    description: 'Nombre del producto',
-    example: 'Smartphone X10',
-  })
-  nombre?: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @Optional()
-  @ApiProperty({
-    description: 'Descripción detallada del producto',
-    example:
-      'Smartphone de última generación con cámara de 48MP y 128GB de almacenamiento',
-  })
-  descripcion?: string;
-
-  @IsNumber({ maxDecimalPlaces: 2 })
-  @Optional()
-  @ApiProperty({
-    description: 'Precio del producto en la moneda local',
-    example: 599.99,
-  })
-  @Min(0)
-  precio?: number;
-
-  @IsInt()
-  @Optional()
-  @ApiProperty({
-    description: 'Cantidad disponible en inventario',
-    example: 50,
-    type: 'integer',
-  })
-  @Min(0)
-  stock?: number;
-}
+export class BaseProductDto
+  extends PartialType(CreateProductDto)
+  implements IBaseSharedProduct {}
